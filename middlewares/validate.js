7e8917e55171809c5e5fcc9b0e96e2c4364e5961
nicovidal <nico.vidal.m@gmail.com>
2023-08-19 22:36:01 -0400

    middleware validate fields

---
+++ middlewares/validate.js
@@ -0,0 +1,22 @@
+const { response } = require("express");
+const { validationResult } = require('express-validator');
+
+const validate=(req,res = response,next)=>{
+
+    const errors=validationResult(req);
+
+    if(!errors.isEmpty()){
+     return res.status(400).json({
+         ok:false,
+         erros:errors.mapped(),
+     })
+    }
+
+    next();
+
+}
+
+
+module.exports={
+    validate,
+}
--- routes/task.js
+++ routes/task.js
@@ -1,13 +1,23 @@
 const {Router}=require("express");
 const { getTask, createTask, updateTask, deleteTask } = require("../controllers/task");
+const { validate } = require("../middlewares/validate");
+const {check}=require('express-validator')
 const router=Router();
+const { isDate } = require("../helpers/isDate");
 
 
 //GetTasks
 router.get('/',getTask)
 
 //Create new Task
-router.post('/',createTask)
+router.post('/',
+[
+    check('description','la descripcion es obligatoria').not().isEmpty(),
+    check('startDate','Fecha de inicio es obligatoria').custom(isDate),
+    check('endDate','Fecha de finalizacion es obligatoria').custom(isDate),
+    validate
+]
+,createTask)
 
 //update Task
 router.put('/:id',updateTask)
